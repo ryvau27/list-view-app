6b1bb00bc50ab7467b59c073be6c2fe31f8b9297
ryvau27 <ryvaughn2701@gmail.com>
2026-06-21 22:20:06 -0400

    forgot to stage changes

--- app/src/main/java/com/example/listviewapp/Item.java
+++ app/src/main/java/com/example/listviewapp/Item.java
@@ -1,4 +1,40 @@
 package com.example.listviewapp;
 
-public class Item {
+import java.io.Serializable;
+
+/*
+ * class representing an item displayed in the ListView.
+ *
+ * Serializable allows this object to be converted into a format
+ * that Android can transfer between Activities using an Intent.
+ */
+public class Item implements Serializable {
+
+    // stores the title
+    private String title;
+
+    // stores the subtitle
+    private String subtitle;
+
+
+    /*
+     * Constructor
+     *
+     * @param title The item name
+     * @param subtitle Additional information about the item
+     */
+    public Item(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    // title getter
+    public String getTitle() {
+        return title;
+    }
+
+    // subtitle getter
+    public String getSubtitle() {
+        return subtitle;
+    }
 }
